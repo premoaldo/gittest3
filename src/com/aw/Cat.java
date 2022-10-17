0bcd7c341cbfa999cf61a03aebe2c8bc68456268
Pedro Remoaldo <premoaldo@gmail.com>
2022-10-17 15:00:52 +0100

    My first branch commit

---
+++ src/com/aw/Cat.java
@@ -0,0 +1,11 @@
+package com.aw;
+
+public class Cat {
+    private String name;
+    private String breed;
+
+    public Cat(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+}
