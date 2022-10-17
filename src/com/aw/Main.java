1ef81b3c709914d8a8f570832f680cc656c01afe
Pedro Remoaldo <premoaldo@gmail.com>
2022-10-17 13:51:54 +0100

    Update Main.java

--- src/com/aw/Main.java
+++ src/com/aw/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Person firstPerson = new Person("Pedro Remoaldo", "Portugal");
         System.out.println(firstPerson.getName());
+        System.out.println(firstPerson.getCountry());
     }
 }
